371238bc6169664d0bd1dd251dae573817f3ebd0
Stephen <1110347+jkgtw@users.noreply.github.com>
2020-08-14 21:09:24 +0800

    Update SSID-DNS.js

--- JS/SSID-DNS.js
+++ JS/SSID-DNS.js
@@ -1,3 +1,13 @@
+/*
+讓 Surge 根據 SSID 來決定要不要使用你設定的 DoH 伺服器
+[General]
+doh-server = 你 DoH 伺服器
+[Host]
+* = script:SSID-DNS
+[Script]
+SSID-DNS = type=dns,script-path=SSID-DNS.js
+*/
+
 if ($network.wifi.ssid === 'SSID1' || $network.wifi.ssid === 'SSID2') {
 $done({servers:$network.dns})
 } else {
