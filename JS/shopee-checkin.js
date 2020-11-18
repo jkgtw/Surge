5d66650638888a48966dc2d8afaefe45aef7f0d1
Stephen <1110347+jkgtw@users.noreply.github.com>
2020-11-18 14:43:06 +0800

    Create shopee-checkin.js

---
+++ JS/shopee-checkin.js
@@ -0,0 +1,39 @@
+/*
+修改自 https://github.com/langkhach270389/Scripting/blob/master/shopee_checkin.js
+
+適用台灣蝦皮 shopee.tw
+
+[Script]
+cron "0 7 * * *" script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-checkin.js
+shopee-cookie.js = type=http-request,pattern=^https:\/\/shopee\.tw\/me\/setting,script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-cookie.js,script-update-interval=-1
+
+MITM = shopee.tw
+*/
+  var shopeeUrl = {
+    url: 'https://shopee.tw/mkt/coins/api/v2/checkin',
+    headers: {
+      Cookie: $persistentStore.read("CookieSP"),
+    }
+  }
+$httpClient.post(shopeeUrl, function(error, response, data){
+  if (error) {
+$notification.post("蝦皮簽到", "", "連線錯誤‼️")
+    $done(); 
+  } 
+ else{
+ if(response.status == 200)
+{
+let obj= JSON.parse(data);
+if(obj["data"]["success"])
+{
+var user = obj["data"]["username"];
+var coins = obj["data"]["increase_coins"];
+$notification.post("蝦皮 " + user, "", "已領取 " + coins + "💰");
+    $done();
+}
+}
+else{
+$notification.post("蝦皮 Cookie 已過期‼️", "", "請重新登入 🔓");
+}
+}
+});
