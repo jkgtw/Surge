56e119e96f96c7830016bedd5a947659d947a31a
Stephen <1110347+jkgtw@users.noreply.github.com>
2021-07-26 09:35:25 +0800

    Update shopee-checkin.js

--- JS/shopee-checkin.js
+++ JS/shopee-checkin.js
@@ -29,7 +29,7 @@ if(obj["data"]["success"])
 var user = obj["data"]["username"];
 var coins = obj["data"]["increase_coins"];
 var checkinday = obj["data"]["check_in_day"];
-$notification.post("蝦皮 " + user + " 已連續簽到 " + checkinday + " 天", "", "今日已領取 " + coins + "💰💰💰");
+$notification.post("蝦皮購物 " + user + " 已連續簽到 " + checkinday + " 天", "", "今日已領取：" + coins + " 蝦幣 💰💰💰");
     $done();
 }
 }
