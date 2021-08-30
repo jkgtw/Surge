90e1b2854d112d1bc884e58417e53c10ee61c21b
Stephen <1110347+jkgtw@users.noreply.github.com>
2021-08-31 07:57:54 +0800

    Update shopee-cookie.js

--- JS/shopee-cookie.js
+++ JS/shopee-cookie.js
@@ -1,13 +1,18 @@
 /*
-修改自 https://github.com/langkhach270389/Scripting/blob/master/shopee_cookie.js
+
+修改自 https://github.com/langkhach270389/Scripting/blob/master/shopee_checkin.js
 
 適用台灣蝦皮 shopee.tw
-安裝啟用後，請使用瀏覽器到以下網址進行登入，保存 Cookie
-https://shopee.tw/me/setting
 
+[Script]
+cron "0 7 * * *" script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-checkin.js, wake-system=1, timeout=30
+cron "1 7 * * *" script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-luckydraw.js, wake-system=1, timeout=30
 shopee-cookie.js = type=http-request,pattern=^https:\/\/shopee\.tw\/me\/setting,script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-cookie.js,script-update-interval=-1
+shopee-loyalty-cookie.js = type=http-request,pattern=^https:\/\/loyalty\.shopee\.tw\/api\/v1\/coins$,script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-cookie.js,script-update-interval=-1
 
-MITM = shopee.tw
+[MITM]
+hostname = %APPEND% loyalty.shopee.tw
+
 */
 
 if ($request.headers['Cookie']) {
