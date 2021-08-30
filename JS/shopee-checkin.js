2678ea23b4db4978c17826f8eea285a83fd5b028
Stephen <1110347+jkgtw@users.noreply.github.com>
2021-08-31 07:58:06 +0800

    Update shopee-checkin.js

--- JS/shopee-checkin.js
+++ JS/shopee-checkin.js
@@ -1,13 +1,18 @@
 /*
+
 修改自 https://github.com/langkhach270389/Scripting/blob/master/shopee_checkin.js
 
 適用台灣蝦皮 shopee.tw
 
 [Script]
-cron "0 7 * * *" script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-checkin.js
+cron "0 7 * * *" script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-checkin.js, wake-system=1, timeout=30
+cron "1 7 * * *" script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-luckydraw.js, wake-system=1, timeout=30
+shopee-cookie.js = type=http-request,pattern=^https:\/\/shopee\.tw\/me\/setting,script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-cookie.js,script-update-interval=-1
 shopee-loyalty-cookie.js = type=http-request,pattern=^https:\/\/loyalty\.shopee\.tw\/api\/v1\/coins$,script-path=https://raw.githubusercontent.com/jkgtw/Surge/master/JS/shopee-cookie.js,script-update-interval=-1
 
-MITM = loyalty.shopee.tw
+[MITM]
+hostname = %APPEND% loyalty.shopee.tw
+
 */
   var shopeeUrl = {
     url: 'https://shopee.tw/mkt/coins/api/v2/checkin',
