1edd5adcd48f308b19fe53afb410c8bb9b271fed
Stephen <1110347+jkgtw@users.noreply.github.com>
2022-04-11 10:21:20 +0800

    Update books-checkin.js

--- JS/books-checkin.js
+++ JS/books-checkin.js
@@ -3,7 +3,7 @@ var updatelpkUrl = {
   headers: {
     Cookie: $persistentStore.read("CookieBooks"),
     "User-Agent":
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 AppVersion:1.0.0 ios_app_shop_ajknrs_2020 support_linepay newAppleLogin",
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 (vsp8.8.0ve) ios_app_shop_ajknrs_2020 support_linepay newAppleLogin newFBLogin newLINELogin",
   },
 };
 
