7bbfa0b92ae4de31fc5c5cd08e557a1fdfaa7e83
Stephen <1110347+jkgtw@users.noreply.github.com>
2022-07-12 18:53:15 +0800

    Update books-cookie.js

--- JS/books-cookie.js
+++ JS/books-cookie.js
@@ -1,12 +1,8 @@
-if ($request.headers["Cookie"] || $request.headers["cookie"]) {
-  var headerBooks = $request.headers["Cookie"] || $request.headers["cookie"];
-  var lpkBooks = headerBooks.indexOf("lpk=");
-  if (lpkBooks > 0) {
-    $done({});
-  } else {
-    $persistentStore.write(headerBooks, "CookieBooks");
-    $notification.post("📗 博客來 Cookie 保存成功 🎉", "", "");
-  }
+const cookie = $request.headers["Cookie"] || $request.headers["cookie"];
+
+if (cookie) {
+  $persistentStore.write(cookie, "CookieBooks");
+  $notification.post("📗 博客來 Cookie 保存成功 🎉", "", "");
 } else {
   $notification.post("📗 博客來 Cookie 保存失敗‼️", "", "請重新登入");
 }
